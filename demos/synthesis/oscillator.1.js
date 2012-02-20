1a6f6b33924e025fcd762352730ab325758a573e
Mike Hodnick <mike.hodnick@gmail.com>
2012-02-20 09:25:39 -0600

    modified oscillator demo

--- demos/synthesis/oscillator.1.js
+++ demos/synthesis/oscillator.1.js
@@ -1,21 +1,21 @@
 (function () {
-	var module = {};
 	
 	var device, 
 		oscillator,
 		playing,
+		distortion
 		channels = 2;
 	
 	$(document).ready(function () {
 		device = audioLib.AudioDevice(audioCallback, channels);
 		oscillator = audioLib.Oscillator(device.sampleRate, 440);
-
+		distortion = audioLib.Distortion(44100);
+		
 		$('#sinePlayButton').click(function () {
 			playing = !playing;
 			$('#sinePlayButton').text(playing ? 'Stop' : 'Play');
 		});
-	});
-	
+	});	
 	
 	function audioCallback(buffer, channelCount) {
 		if (playing) {
